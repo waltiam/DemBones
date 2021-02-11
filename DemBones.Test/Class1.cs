9dba008a3527a29cbfee1f7dcc90514dd8ba995f
walt <2963975+waltiam@users.noreply.github.com>
2021-02-11 00:56:50 +0000

    project setup and code migration

---
+++ DemBones.Test/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DemBones.Test
+{
+    public class Class1
+    {
+    }
+}
---
+++ DemBones/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DemBones
+{
+    public class Class1
+    {
+    }
+}
